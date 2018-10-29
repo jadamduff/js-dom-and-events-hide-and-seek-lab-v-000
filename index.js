260bd91a0b214b901f76b74830b9f7f66b0db9a8
Adam Duff <j.adam.duff@gmail.com>
2018-10-29 10:04:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ function nestedTarget() {
 function deepestChild() {
   return document.querySelector('div#grand-node div div div div');
 }
+
+function increaseRankBy(n) {
+  let lists = document.querySelectorAll('div.ranked-list')
+  for (const list of lists) {
+    for (const rank of list) {
+      rank.innerHTML(parseInt(rank.innerHTML) + n);
+    }
+  }
+}
